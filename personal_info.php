23f2a60928109a1236f59916effb7ed0ee2f87e0
zisan2108 <44030115+zisan2108@users.noreply.github.com>
2023-09-12 19:30:52 +0600

    Task 1: Personal Information Page

---
+++ personal_info.php
@@ -0,0 +1,11 @@
+<?php
+
+$name ="Nahid";
+$age =24;
+$country ="Bangladesh";
+$brief_introduction =" Currently I am learning PHP and Laravel. My goal is to become a Senior Laravel developer.";
+
+echo"My Self: ".$name."\n",
+"My age is: ".$age. "\n", 
+"My Country name is: ".$country. "\n", 
+"Introduction:".$brief_introduction. "\n" ;
